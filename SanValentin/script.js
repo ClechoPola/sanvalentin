0de0be01c2783fe336f4ecd7a2f7b61835a0c271
Cleynathan <159574425+ClechoPola@users.noreply.github.com>
2025-02-13 18:13:21 -0400

    Add files via upload

---
+++ SanValentin/script.js
@@ -0,0 +1,16 @@
+const noBtn = document.getElementById('no-btn');
+const siBtn = document.getElementById('si-btn');
+const loroImg = document.getElementById('loro-img');
+
+noBtn.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
+
+siBtn.addEventListener('click', () => {
+    loroyImg.src = 'bird_love.gif'; // Cambia el GIF de loro a uno feliz
+    alert('Te amo :3');
+});
